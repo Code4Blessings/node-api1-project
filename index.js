ee3e6abae0d235d0ed1c941cffb744c27e5581d5
Code4Blessings <robinwarden@code4blessings.com>
2020-01-13 18:51:01 -0500

    Added a get users endpoint to server

--- index.js
+++ index.js
@@ -5,12 +5,22 @@ const dBase = require('./data/db');
 
 const server = express();
 
+server.use(express.json())
+
 server.get('/', (req, res) => {
     res.send({Greeting: 'Hello from dBase!'});
 });
 
 server.get('/api/users', (req, res) => {
-    const users = 
+    dBase.find()
+    .then(users => {
+        res.status(200).json(users);
+    })
+    .catch(err => {
+        res.status(500).json({
+            errorMessage: "Failed to find users"
+        })
+    })
 })
 
 server.listen(5000, () => console.log('API running on port 5000'));
