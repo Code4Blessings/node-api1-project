93ed1c4f644eb8a30c53cc42ec06d4d52189a577
Code4Blessings <robinwarden@code4blessings.com>
2020-01-13 23:41:47 -0500

    Made an Get User By ID endpoint

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ server.get('/', (req, res) => {
     res.send({Greeting: 'Hello from dBase!'});
 });
 
+//Get a list of all users in database
+
 server.get('/api/users', (req, res) => {
     dBase.find()
     .then(users => {
@@ -18,9 +20,35 @@ server.get('/api/users', (req, res) => {
     })
     .catch(err => {
         res.status(500).json({
-            errorMessage: "Failed to find users"
+            errorMessage: "The users information could not be retrieved."
         })
     })
 })
+
+//Get User By Id
+
+server.get('/api/users/:id', (req, res) => {
+    const id = req.params.id;
+    dBase.findById(id)
+    .then(user => {
+        if(!user) {
+            res.status(404).json({
+                errorMessage: "The user with the specified ID does not exist."
+            })
+        }
+        res.status(200).json(user);
+    }) 
+    .catch(err => {
+        res.status(404).json({
+            errorMessage: "The user with the specified ID does not exist."
+        })
+    })
+})
+
+//Create a user
+
+// server.post('/api/users', (req, res) => {
+//     const userData = req.body;
+// })
 
 server.listen(5000, () => console.log('API running on port 5000'));
